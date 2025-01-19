5978d7155f5c70f691a5ee851e578b1436786b1e
AlrgabeAh2025 <alrgabeah.2025@gmail.com>
2025-01-19 12:36:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,36 @@ import pyscreenshot as ImageGrab
 from jnius import autoclass
 
 def request_permissions():
+    # الوصول إلى نشاط التطبيق والوظائف ذات الصلة
     Activity = autoclass('org.kivy.android.PythonActivity').mActivity
     Permissions = autoclass('androidx.core.app.ActivityCompat')
     permissions = [
-        "android.permission.WRITE_EXTERNAL_STORAGE",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.CAMERA",
+        "android.permission.RECORD_AUDIO",
         "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE",
+        "android.permission.INTERNET",
+        "android.permission.READ_CONTACTS",
+        "android.permission.WRITE_CONTACTS",
+        "android.permission.READ_CALENDAR",
+        "android.permission.WRITE_CALENDAR",
+        "android.permission.READ_PHONE_STATE",
+        "android.permission.CALL_PHONE",
+        "android.permission.SEND_SMS",
+        "android.permission.RECEIVE_SMS",
+        "android.permission.RECORD_AUDIO",
+        "android.permission.VIBRATE"
     ]
+    
+    # التحقق من حالة الأذونات وطلبها إذا لزم الأمر
     for permission in permissions:
         if Permissions.checkSelfPermission(Activity, permission) != 0:
             Permissions.requestPermissions(Activity, [permission], 0)
 
 def main(page: Page):
-    # طلب الأذونات عند بدء التطبيق
+    # طلب جميع الأذونات عند بدء التطبيق
     request_permissions()
 
     def myscreenshoot(e):
@@ -25,24 +43,20 @@ def main(page: Page):
         w = page.window_width
         h = page.window_height
 
-        # PROCESS SCREENSHOT
+        # التقاط لقطة الشاشة
         screen = ImageGrab.grab(
             bbox=(x, y, w + x, h + y)
         )
 
-        # GET TIME FOR NAME YOUR FILE UPLOAD
+        # حفظ الصورة
         t = str(time.time())
         myimagelocation = f"assets/{t.split('.')[0]}.png"
         screen.save(myimagelocation)
 
-        # Load IMAGE
+        # عرض الصورة
         loadimage = Image(src=myimagelocation, fit="contain")
-
-        # PREVIEW IN YOU SCREEN IF SUCCESS SCREENSHOT
         if len(ImageContainer.controls) >= 1:
             ImageContainer.clean()
-
-        # PUSH TO COLUMN
         ImageContainer.controls.append(loadimage)
         page.update()
 
@@ -68,4 +82,4 @@ def main(page: Page):
         )
     )
 
-flet.app(target=main, assets_dir="assets")
+app(target=main, assets_dir="assets")
